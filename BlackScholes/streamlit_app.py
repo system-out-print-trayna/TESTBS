e5cf11ae17d0a44082ba5c16bce18de4c486a838
Trayna Bùi <115993485+system-out-print-trayna@users.noreply.github.com>
2024-09-17 02:53:44 -0400

    Update streamlit_app.py

--- BlackScholes/streamlit_app.py
+++ BlackScholes/streamlit_app.py
@@ -125,7 +125,7 @@ with st.sidebar:
     st.title("📊 Black-Scholes Model")
     st.write("`Created by:`")
     linkedin_url = "https://www.linkedin.com/in/trayna05/"
-    st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Prudhvi Reddy, Muppala`</a>', unsafe_allow_html=True)
+    st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Trayna Bui`</a>', unsafe_allow_html=True)
 
     current_price = st.number_input("Current Asset Price", value=100.0)
     strike = st.number_input("Strike Price", value=100.0)
